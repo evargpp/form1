e965032b4ac126b25c3454da61c173995c666d48
Twoje Imię i Nazwisko <grave4332@gmail.com>
2025-12-13 18:37:48 +0100

    niby gotowe

--- script.js
+++ script.js
@@ -12,62 +12,7 @@ function updateCount() {
     rowCount.textContent = table.children.length;
 }
 
-function createRowOrg(name = "", age = "", guardian = "") {
-    const tr = document.createElement("tr");
-
-    tr.innerHTML = `
-        <td><input value="${name}"></td>
-        <td>
-            <select>
-                <option value="">-- wybierz --</option>
-                <option value="adult" ${age === "adult" ? "selected" : ""}>dorosły</option>
-                <option value="child" ${age === "child" ? "selected" : ""}>dziecko</option>
-            </select>
-        </td>
-        <td><input ${age !== "child" ? "disabled" : ""} value="${guardian}"></td>
-        <td>
-            <button class="up">↑</button>
-            <button class="down">↓</button>
-            <button class="delete">Usuń</button>
-        </td>
-    `;
-
-    // Logika edycji opiekuna
-    const ageSel = tr.querySelector("select");
-    const guardianInput = tr.querySelector("td:nth-child(3) input");
-
-    ageSel.addEventListener("change", () => {
-        if (ageSel.value === "child") {
-            guardianInput.disabled = false;
-        } else {
-            guardianInput.disabled = true;
-            guardianInput.value = "";
-        }
-    });
-
-    // Usuwanie
-    tr.querySelector(".delete").addEventListener("click", () => {
-        tr.remove();
-        updateCount();
-    });
-
-    // Sortowanie
-    tr.querySelector(".up").addEventListener("click", () => {
-        if (tr.previousElementSibling) {
-            table.insertBefore(tr, tr.previousElementSibling);
-        }
-    });
-
-    tr.querySelector(".down").addEventListener("click", () => {
-        if (tr.nextElementSibling) {
-            table.insertBefore(tr.nextElementSibling, tr);
-        }
-    });
-
-    return tr;
-}
-
-function createRow(name = "", age = "", guardian = "") {
+function createRow() {
     const tr = document.createElement("tr");
 
     tr.innerHTML = `
